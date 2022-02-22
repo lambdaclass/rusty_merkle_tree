93b9c2c00f92bbc2963e8dacb796025e04338313
Iván Litteri <ilitteri@fi.uba.ar>
2022-02-22 11:03:05 -0300

    Update node implementation
    From node struct to string

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,8 @@ use std::collections::VecDeque;
 use std::hash::{Hash, Hasher};
 
 struct MerkleTree {
-    pub nodes: Vec<MerkleTreeNode>,
+    pub nodes: Vec<String>,
     pub root_index: Option<usize>,
-}
-
-struct MerkleTreeNode {
-    pub hash: String,
-}
-
-impl Clone for MerkleTreeNode {
-    fn clone(&self) -> MerkleTreeNode {
-        MerkleTreeNode {
-            hash: self.hash.to_string(),
-        }
-    }
 }
 
 impl MerkleTree {
@@ -33,7 +21,7 @@ impl MerkleTree {
         hashed_input_elements
     }
 
-    fn propagate_hashes(nodes: &mut Vec<MerkleTreeNode>, node_index: usize) {
+    fn propagate_hashes(nodes: &mut Vec<String>, node_index: usize) {
         MerkleTree::propagate_hashes(nodes, MerkleTree::left_child_index(node_index));
         MerkleTree::propagate_hashes(nodes, MerkleTree::right_child_index(node_index));
     }
@@ -51,11 +39,9 @@ impl MerkleTree {
         T: Hash,
     {
         let input_elements = MerkleTree::build_hashes(input_elements);
-        let mut nodes: Vec<MerkleTreeNode> = Vec::with_capacity(input_elements.len());
+        let mut nodes = Vec::with_capacity(input_elements.len());
         for elem in input_elements.iter() {
-            nodes.push(MerkleTreeNode {
-                hash: elem.to_string(),
-            });
+            nodes.push(elem.to_string());
         }
 
         MerkleTree::propagate_hashes(&mut nodes, 1);
@@ -63,12 +49,8 @@ impl MerkleTree {
         while input_elements.len() >= 2 {
             let left_hash = input_elements.pop_front().unwrap();
             let right_hash = input_elements.pop_front().unwrap();
-            nodes.push(MerkleTreeNode {
-                hash: left_hash.to_string(),
-            });
-            nodes.push(MerkleTreeNode {
-                hash: right_hash.to_string(),
-            });
+            nodes.push(left_hash.to_string());
+            nodes.push(right_hash.to_string());
             input_elements.push_back(left_hash + &right_hash);
         }
 
