ab5b87b988ecf3fd57a1093329aab99cc1c8d9ac
IAvecilla <iavecilla@fi.uba.ar>
2022-02-22 10:06:08 -0300

    fixes for compiler errors, now builds succesfully

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use std::collections::VecDeque;
-use std::hash::Hash;
 use std::collections::hash_map::DefaultHasher;
+use std::collections::VecDeque;
+use std::hash::{Hash, Hasher};
 
 struct MerkleTree {
     pub nodes: Vec<MerkleTreeNode>,
@@ -11,20 +11,29 @@ struct MerkleTreeNode {
     pub hash: String,
 }
 
-impl MerkleTree {
+impl Clone for MerkleTreeNode {
+    fn clone(&self) -> MerkleTreeNode {
+        MerkleTreeNode {
+            hash: self.hash.to_string(),
+        }
+    }
+}
 
+impl MerkleTree {
     fn build_hashes<T>(input_elements: Vec<T>) -> Vec<String>
-    where T: Hash {
-        let hashed_input_elements: Vec<String> = Vec::new();
+    where
+        T: Hash,
+    {
+        let mut hashed_input_elements: Vec<String> = Vec::new();
         let mut hasher = DefaultHasher::new();
         for element in input_elements.iter() {
-            hashed_input_elements.push(element.hash(&mut hasher));
+            element.hash(&mut hasher);
+            hashed_input_elements.push(hasher.finish().to_string());
         }
         hashed_input_elements
     }
 
-    fn propagete_hashes<T>(nodes: &mut Vec<T>, node_index: usize) 
-    where T: Hash { 
+    fn propagate_hashes(nodes: &mut Vec<MerkleTreeNode>, node_index: usize) {
         MerkleTree::propagate_hashes(nodes, MerkleTree::left_child_index(node_index));
         MerkleTree::propagate_hashes(nodes, MerkleTree::right_child_index(node_index));
     }
@@ -37,43 +46,41 @@ impl MerkleTree {
         parent_index * 2 + 1
     }
 
-    pub fn new_from<T>(input_elements: Vec<T>) -> MerkleTree 
-    where T: std::fmt::Display + Hash {
-        let input_elements = MerkleTree::build_hashes(input_elements.clone());
-        let mut nodes = Vec::with_capacity(input_elements.len());
+    pub fn new_from<T>(input_elements: Vec<T>) -> Self
+    where
+        T: Hash,
+    {
+        let input_elements = MerkleTree::build_hashes(input_elements);
+        let mut nodes: Vec<MerkleTreeNode> = Vec::with_capacity(input_elements.len());
         for elem in input_elements.iter() {
-            nodes.push(elem);
+            nodes.push(MerkleTreeNode {
+                hash: elem.to_string(),
+            });
         }
 
         MerkleTree::propagate_hashes(&mut nodes, 1);
-        let input_elements = VecDeque::from(input_elements);
+        let mut input_elements = VecDeque::from(input_elements);
         while input_elements.len() >= 2 {
             let left_hash = input_elements.pop_front().unwrap();
             let right_hash = input_elements.pop_front().unwrap();
             nodes.push(MerkleTreeNode {
-                hash: left_hash,
+                hash: left_hash.to_string(),
             });
-            nodes.push(MerkleTreeNode { 
-                hash: right_hash 
+            nodes.push(MerkleTreeNode {
+                hash: right_hash.to_string(),
             });
-            input_elements.push_back(left_hash, right_hash);
+            input_elements.push_back(left_hash + &right_hash);
         }
 
         MerkleTree {
-            nodes,
+            nodes: nodes.clone(),
             root_index: Some(nodes.len() - 1),
         }
     }
 
-    pub fn proof() {
-        
-    }
+    pub fn proof() {}
 
-    pub fn verify() {
-        
-    }
+    pub fn verify() {}
 }
 
-fn main() {
-    
-}
+fn main() {}
